75dc2e232c325195497403915ebd1efd1f73d942
Kehinde <giwatwins@gmail.com>
2025-08-17 23:03:06 +0100

    merge

--- Backend/SearchEngine.DocumentProcessing/Parsers/PlainTextParser.cs
+++ Backend/SearchEngine.DocumentProcessing/Parsers/PlainTextParser.cs
@@ -2,19 +2,19 @@ using SearchEngine.DocumentProcessing.Interfaces;
 
 namespace SearchEngine.DocumentProcessing.Parsers
 {
-  /// <summary>
-  /// Parser for reading content from TXT files
-  /// </summary>
-  public class PlainTextParser : IParser
-  {
     /// <summary>
-    /// Reads the content of a TXT file
+    /// Parser for reading content from TXT files
     /// </summary>
-    /// <param name="filePath">The path to the TXT file</param>
-    /// <returns>The plain text content of the file</returns>
-    public string ReadContent(string filePath)
+    public class PlainTextParser : IParser
     {
-      return File.ReadAllText(filePath);
+        /// <summary>
+        /// Reads the content of a TXT file
+        /// </summary>
+        /// <param name="filePath">The path to the TXT file</param>
+        /// <returns>The plain text content of the file</returns>
+        public string ReadContent(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
     }
-  }
 }
---
+++ Backend/SearchEngine.Tests/SearchEngine.DocumentProcessing.Tests/Parsers/ParserTest.cs
@@ -0,0 +1,16 @@
+using SearchEngine.DocumentProcessing.Parsers;
+namespace SearchEngine.Tests.Parsers
+{
+    public class PlainTextParserTest
+    {
+        [Fact]
+        public void ReadContent_ValidTxt_ReturnsText()
+        {
+            // Arrange
+            var filePath = "text.txt";
+            // Act
+            // string content = PlainTextParser.
+            // Assert
+        }
+    }
+}
